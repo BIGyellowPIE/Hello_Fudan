cbe17977471820c619d3c677deb0543499252a78
BIGyellowPIE <43981527+BIGyellowPIE@users.noreply.github.com>
2022-11-21 12:14:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -264,137 +264,6 @@ class Zlapp(Fudan):
             if(json_loads(save.text)["e"] != 1):
                 break
 
-class Mail1:
-    def __init__(self):
-        self.mail_host = "smtp.qq.com"  # qq邮箱服务器
-        self.mail_pass = "kshwghsboixkdibb"  # 授权码
-        self.sender = 'niequanxin@qq.com'  # 发送方邮箱地址
-        self.receivers = ['929902277@qq.com']  # 收件人的邮箱地址
-
-    def send(self):
-        content = '最爱🧣的🫓已经帮忙打卡成功啦！\n现在是北京时间：{} 一直在很喜欢很喜欢很喜欢你哦～n'.format(time.ctime())
-        message = MIMEText(content, 'plain', 'utf-8')
-        message['From'] = Header("饼饼的平安复旦打卡提醒", 'utf-8')
-        message['To'] = Header("User", 'utf-8')
-        subject = '平安复旦每日打卡'  # 发送的主题
-        message['Subject'] = Header(subject, 'utf-8')
-        try:
-            smtpObj = smtplib.SMTP_SSL(self.mail_host, 465)
-            smtpObj.login(self.sender, self.mail_pass)
-            smtpObj.sendmail(self.sender, self.receivers, message.as_string())
-            smtpObj.quit()
-            print('\n邮件已发送!')
-        except smtplib.SMTPException as e:
-            pass
-            print('\n邮件发送失败!')
-
-class Zlapp1(Fudan):
-    last_info = ''
-
-    def check(self):
-        """
-        检查
-        """
-        print("◉检测是否已提交")
-        get_info = self.session.get(
-            'https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info')
-        last_info = get_info.json()
-
-        print("◉上一次提交日期为:", last_info["d"]["info"]["date"])
-
-        position = last_info["d"]["info"]['geo_api_info']
-        position = json_loads(position)
-
-        print("◉上一次提交地址为:", position['formattedAddress'])
-        # print("◉上一次提交GPS为", position["position"])
-        # print(last_info)
-        
-        # 改为上海时区
-        os.environ['TZ'] = 'Asia/Shanghai'
-        time.tzset()
-        today = time.strftime("%Y%m%d", time.localtime())
-        print("◉今日日期为:", today)
-        if last_info["d"]["info"]["date"] == today:
-            print("\n*******今日已提交*******")
-            mail = Mail1()
-            mail.send()
-            self.close()
-        else:
-            print("\n\n*******未提交*******")
-            self.last_info = last_info["d"]["oldInfo"]
-            
-    def read_captcha(self, img_byte):
-        img = Image.open(io.BytesIO(img_byte)).convert('L')
-        enh_bri = ImageEnhance.Brightness(img)
-        new_img = enh_bri.enhance(factor=1.5)
-        
-        image = numpy.array(new_img)
-        reader = easyocr.Reader(['en'])
-        horizontal_list, free_list = reader.detect(image, optimal_num_chars=4)
-        character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        allow_list = list(character)
-        allow_list.extend(list(character.lower()))
-    
-        result = reader.recognize(image, 
-                                allowlist=allow_list,
-                                horizontal_list=horizontal_list[0],
-                                free_list=free_list[0],
-                                detail = 0)
-        return result[0]
-    
-
-    def validate_code(self):
-        img = self.session.get(self.url_code).content
-        return self.read_captcha(img)
-
-    def checkin(self):
-        """
-        提交
-        """
-        headers = {
-            "Host": "zlapp.fudan.edu.cn",
-            "Referer": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history",
-            "DNT": "1",
-            "TE": "Trailers",
-            "User-Agent": self.UA
-        }
-
-        print("\n\n◉◉提交中")
-
-        geo_api_info = json_loads(self.last_info["geo_api_info"])
-        province = self.last_info["province"]
-        city = self.last_info["city"]
-        district = geo_api_info["addressComponent"].get("district", "")
-        
-        while(True):
-            print("◉正在识别验证码......")
-            code = self.validate_code()
-            print("◉验证码为:", code)
-            self.last_info.update(
-                {
-                    "tw": "13",
-                    "province": province,
-                    "city": city,
-                    "area": " ".join((province, city, district)),
-                    #"sfzx": "1",  # 是否在校
-                    #"fxyy": "",  # 返校原因
-                    "code": code,
-                }
-            )
-            # print(self.last_info)
-            save = self.session.post(
-                'https://zlapp.fudan.edu.cn/ncov/wap/fudan/save',
-                data=self.last_info,
-                headers=headers,
-                allow_redirects=False)
-
-            save_msg = json_loads(save.text)["m"]
-            print(save_msg, '\n\n')
-            time.sleep(0.1)
-            if(json_loads(save.text)["e"] != 1):
-                break
-                
-                
 def get_account():
     """
     获取账号信息
@@ -427,73 +296,21 @@ def get_account():
 
     return uid, psw
 
-def get_account1():
-    """
-    获取账号信息
-    """
-    uid = getenv("STD_ID1")
-    psw = getenv("PASSWORD1")
-    if uid != None and psw != None:
-        print("从环境变量中获取了用户名和密码！")
-        return uid, psw
-    print("\n\n请仔细阅读以下日志！！\n请仔细阅读以下日志！！！！\n请仔细阅读以下日志！！！！！！\n\n")
-    if os_path.exists("account.txt"):
-        print("读取账号中……")
-        with open("account.txt", "r") as old:
-            raw = old.readlines()
-        if (raw[0][:3] != "uid") or (len(raw[0]) < 10):
-            print("account.txt 内容无效, 请手动修改内容")
-            sys_exit()
-        uid = (raw[0].split(":"))[1].strip()
-        psw = (raw[1].split(":"))[1].strip()
-
-    else:
-        print("未找到account.txt, 判断为首次运行, 请接下来依次输入学号密码")
-        uid = input("学号：")
-        psw = getpass("密码：")
-        with open("account.txt", "w") as new:
-            tmp = "uid:" + uid + "\npsw:" + psw +\
-                "\n\n\n以上两行冒号后分别写上学号密码，不要加空格/换行，谢谢\n\n请注意文件安全，不要放在明显位置\n\n可以从dailyFudan.exe创建快捷方式到桌面"
-            new.write(tmp)
-        print("账号已保存在目录下account.txt，请注意文件安全，不要放在明显位置\n\n建议拉个快捷方式到桌面")
-
-    return uid, psw
-
-
 
 if __name__ == '__main__':
-    while True:
-        uid, psw = get_account()
-        # print(uid, psw)
-        zlapp_login = 'https://uis.fudan.edu.cn/authserver/login?' \
-                      'service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily'
-        code_url = "https://zlapp.fudan.edu.cn/backend/default/code"
+    uid, psw = get_account()
+    # print(uid, psw)
+    zlapp_login = 'https://uis.fudan.edu.cn/authserver/login?' \
+                  'service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily'
+    code_url = "https://zlapp.fudan.edu.cn/backend/default/code"
+    
+    print("当前脚本执行时间：", time.ctime())
+    daily_fudan = Zlapp(uid, psw,
+                        url_login=zlapp_login, url_code=code_url)
+    daily_fudan.login()
 
-        print("当前脚本执行时间：", time.ctime())
-        daily_fudan = Zlapp(uid, psw,
-                            url_login=zlapp_login, url_code=code_url)
-        daily_fudan.login()
-
-        daily_fudan.check()
-        daily_fudan.checkin()
-        # 再检查一遍
-        daily_fudan.check()
-        daily_fudan.close(1)
-
-        uid, psw = get_account1()
-        # print(uid, psw)
-        zlapp_login = 'https://uis.fudan.edu.cn/authserver/login?' \
-                      'service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily'
-        code_url = "https://zlapp.fudan.edu.cn/backend/default/code"
-
-        print("当前脚本执行时间：", time.ctime())
-        daily_fudan1 = Zlapp1(uid, psw,
-                            url_login=zlapp_login, url_code=code_url)
-        daily_fudan1.login()
-
-        daily_fudan1.check()
-        daily_fudan1.checkin()
-        # 再检查一遍
-        daily_fudan1.check()
-        daily_fudan1.close(1)
-        break
+    daily_fudan.check()
+    daily_fudan.checkin()
+    # 再检查一遍
+    daily_fudan.check()
+    daily_fudan.close(1)
